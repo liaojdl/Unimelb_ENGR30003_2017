e101dd0b70d40fb70a733d538e7392112740729a
liaojdl <37972702+liaojdl@users.noreply.github.com>
2021-03-06 12:10:08 +1100

    Assignment 2 and associated report

---
+++ Assignment 2/tasks.c
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *
+ *   File        : tasks.c
+ *   Student Id  : <INSERT STUDENT ID HERE>
+ *   Name        : <INSERT STUDENT NAME HERE>
+ *
+ ***************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <string.h>
+#include "tasks.h"
+
+void shockwave(const char* q2_file)
+{
+    printf("shockwave() - IMPLEMENT ME!\n");
+    exit(EXIT_FAILURE);
+}
+
+void linalgbsys(const char* q4_file)
+{
+    printf("linalgbsys() - IMPLEMENT ME!\n");
+    exit(EXIT_FAILURE);
+}
+
+void interp(const char* q5_file, const double xo)
+{
+    printf("interp() - IMPLEMENT ME!\n");
+    exit(EXIT_FAILURE);
+}
+
+void heateqn(const char* q6_file)
+{
+    printf("heateqn() - IMPLEMENT ME!\n");
+    exit(EXIT_FAILURE);
+}
